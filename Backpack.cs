702452859cd089dcbd0a233248e1e4f98c52fc35
GrumpyCrouton <12193403+GrumpyCrouton@users.noreply.github.com>
2024-03-27 22:18:06 -0500

    Address warnings

--- Backpack.cs
+++ Backpack.cs
@@ -24,17 +24,15 @@ namespace SmartPocketBoxes
         public bool setup = false;
 
         public Rack rack;
-        private RackSlot slot;
 
-        private BoxInteraction _boxInteraction;
-
+        //this is left here to remove the old backpack rack that will no longer be used.
+        //will remove eventually
         public void Setup(Plugin plugin, ManualLogSource logger)
         {
             _plugin = plugin;
             _logger = logger;
             setup = true;
 
-            _logger.LogError("Getting Rack Manager");
             RackManager rack_manager = FindFirstObjectByType<RackManager>();
 
             foreach(Rack existing_rack in rack_manager.m_Racks)
